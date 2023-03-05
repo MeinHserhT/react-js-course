190bdf3a21f9ee35f9123e5efbe670847d8af9da
MeinHserhT <vyminh0404@gmail.com>
2023-03-05 23:10:24 +0700

    Add files via upload

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import './blog.css';
+import { Article } from '../../components';
+import {blog01, blog02, blog03, blog04, blog05} from './imports'; 
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className="gradient__text">
+          A lot is happening. We are blogging about it.
+        </h1>
+        <div className="gpt3__blog-container">
+          <div className="gpt3__blog-container_groupA">
+            <Article imgURL={blog01} title='GPT-3 and OpenAI is the future. Let us explore how it is?' date='Sep 25, 2021'/>
+          </div>
+          <div className="gpt3__blog-container_groupB">
+            <Article imgURL={blog02} title='GPT-3 and OpenAI is the future. Let us explore how it is?' date='Sep 25, 2021'/>
+            <Article imgURL={blog03} title='GPT-3 and OpenAI is the future. Let us explore how it is?' date='Sep 25, 2021'/>
+            <Article imgURL={blog04} title='GPT-3 and OpenAI is the future. Let us explore how it is?' date='Sep 25, 2021'/>
+            <Article imgURL={blog05} title='GPT-3 and OpenAI is the future. Let us explore how it is?' date='Sep 25, 2021'/>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
